d659cdec72996bdd0110bc2980ba3db954058016
chomba <chomba@bongohive.co.zm>
2021-12-22 07:31:57 +0200

    can open browser, can print quotes

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    extends: 'eslint:recommended',
+    rules: {
+        // enable additional rules
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'always'],
+
+        // override configuration set by extending "eslint:recommended"
+        'no-empty': 'warn',
+        'no-cond-assign': ['error', 'always'],
+
+        // disable rules from base configurations
+        'for-direction': 'off',
+
+        env: {
+            browser: true,
+            amd: true,
+            node: true,
+        },
+    },
+}
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # shiny-adventure
+
 A node cli tool that prints qoutes in the terminal
+
+## Install
+
+#
+
+```
+npm install -g shiny-adventure or yarn add -g shiny-adventure
+
+
+```
+
+## Run
+
+#
+
+Open terminal on mac or linux , CMD on windows
+
+```js
+shiny-adventure // opens in browser
+
+shiny-adventure --print // prints in the console
+
+```
